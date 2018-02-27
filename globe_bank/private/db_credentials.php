d437058f5da648fb9c30c003b03592379dd7f4d5
William del Rosario <williamdelrosario@yahoo.com>
2018-02-27 15:16:01 +0100

    Blueprint the app

--- globe_bank/private/db_credentials.php
+++ globe_bank/private/db_credentials.php
@@ -1,8 +1,8 @@
 <?php
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "secretpassword");
+define("DB_USER", "root");
+define("DB_PASS", "root");
 define("DB_NAME", "globe_bank");
 
 ?>
