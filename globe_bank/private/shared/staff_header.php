3f494d5ee0f0f9a7d7126463cff74044fdd8dcfe
William del Rosario <williamdelrosario@yahoo.com>
2018-06-07 10:02:06 +0200

    Create admins table

--- globe_bank/private/shared/staff_header.php
+++ globe_bank/private/shared/staff_header.php
@@ -1,25 +1,27 @@
 <?php
-  if(!isset($page_title)) { $page_title = 'Staff Area'; }
+if(!isset($page_title)) { $page_title = 'Staff Area'; }
 ?>
 
 <!doctype html>
 
 <html lang="en">
-  <head>
+<head>
     <title>GBI - <?php echo h($page_title); ?></title>
     <meta charset="utf-8">
     <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css'); ?>" />
-  </head>
+</head>
 
-  <body>
-    <header>
-      <h1>GBI Staff Area</h1>
-    </header>
+<body>
+<header>
+    <h1>GBI Staff Area</h1>
+</header>
 
-    <navigation>
-      <ul>
-          <li>user: <?php echo $_SESSION['username'] ?? ''; ?></li>
+<navigation>
+    <ul>
+        <li>User: <?php echo $_SESSION['username'] ?? ''; ?></li>
         <li><a href="<?php echo url_for('/staff/index.php'); ?>">Menu</a></li>
         <li><a href="<?php echo url_for('/staff/logout.php'); ?>">Logout</a></li>
-      </ul>
-    </navigation>
+    </ul>
+</navigation>
+
+<?php echo display_session_message(); ?>
